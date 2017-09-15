22cf32c1ec9c8fcb6c0928943c3f57ddd2461ae9
Kamil <kamillussa@gmail.com>
2017-09-15 14:32:57 +0200

    bodified index and books.js

--- frontend/index.php
+++ frontend/index.php
@@ -56,7 +56,9 @@
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-
+<script>
+    const API_HOST = "http://localhost/PHP/Bookstore/rest/rest.php";
+</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="js/scripts.js"></script>
---
+++ frontend/js/books.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    //Opis książki    
+    $(document).on('click', '.btn-book-show-description', function () {
+        var bookID = $(this).data('id');
+        var parent = $(this).parent().parent();
+        $.ajax({
+            url: `${API_HOST}/book/${bookID}`,
+            method: 'GET',
+            dataType: 'json'
+        }).done(function (result) {
+            parent.find('.book-description').html(result.success[0].description).show();
+        });
+    });
+    //addBook()
+    function addBook(book) {
+        var element = `<li class="list-group-item">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <span class="bookTitle">${book.title}</span>
+                                <button data-id="${book.id}" class="btn btn-danger pull-right btn-xs btn-book-remove"><i class="fa fa-trash"></i>
+                                </button>
+                                <button data-id="${book.id}" class="btn btn-primary pull-right btn-xs btn-book-show-description"><i class="fa fa-info-circle"></i>
+                                </button>
+                            </div>
+                            <div class="panel-body book-description">${book.description}</div>
+                        </div>
+                    </li>`;
+        $('#booksList').append(element);
+    }
+    //Submit
+    $('#bookAdd').on('submit', function (e) {
+        e.preventDefault();
+        var title = $('#title').val();
+        var description = $('#description').val();
+        if (title.length == 0 && description.length == 0) {
+            return;
+        }
+        $.ajax({
+            url: `${API_HOST}/book`,
+            data: {
+                title: title,
+                description: description
+            },
+            method: "POST",
+            dataType: "json"
+        }).done(function (result) {
+            addBook(result.success[0]);
+        }).fail(function (xhr, cod) {
+            console.log(xhr, cod);
+        })
+    });
+    //Pobranie książek
+    $.ajax({
+        url: `${API_HOST}/book`,
+        method: 'GET',
+        dataType: 'json'
+    }).done(function (result) {
+        result.success.forEach((e) => addBook(e));
+    });
+
+    //Delete Book
+    $(document).on('click', '.btn-book-remove', function () {
+        var bookID = $(this).data('id');
+        var parent = $(this).parent().parent();
+        $.ajax({
+            url: `${API_HOST}/book/${bookID}`,
+            method: 'GET',
+            dataType: 'json'
+        }).done(function (result) {
+            console.log(result.success[0]);
+            parent.find('.book-description').html(result.success[0].description).hide();
+        });
+    });
+});
